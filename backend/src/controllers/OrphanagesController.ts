c315bca0227742ebcd23dbbaaf37cc53f9511b33
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-10-14 17:14:17 -0300

    Corrigindo bug

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -14,7 +14,10 @@ export default {
 
 		const orphanagesRepository = getRepository(Orphanage)
 
-		const data = { name, latitude, longitude, about, instructions, opening_hours, open_on_weekends, images }
+		const data = {
+			name, latitude, longitude, about, instructions, opening_hours,
+			open_on_weekends: open_on_weekends === 'true', images
+		}
 
 		const schema = Yup.object().shape({
 			name: Yup.string().required('O campo "name" é obrigatório.'),
